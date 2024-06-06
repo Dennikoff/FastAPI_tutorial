bc6406101455e7c696b250c63787ba293be45bf6
Денис Харке <hda007@campus.mephi.ru>
2024-06-06 17:06:25 +0300

    add Optional Value

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI(title="Trading App")
 
 users = [
-    {"id": 1, "name": "Alex"},
+    {"id": 1, "name": "Alex", "degree": [
+        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "junior"},
+    ]},
     {"id": 2, "name": "Denis"},
     {"id": 3, "name": "Ilya"},
     {"id": 4, "name": "Andy"}
 ]
 
+class DegreeType(Enum):
+    juniour = "junior"
+    expert = "expert"
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+
+class User(BaseModel):
+    id: int
+    name: str
+    degree: Optional[List[Degree]] = []
+
 @app.get("/users/")
 def get_users():
     return {"users": users}
 
-@app.get("/users/{user_id}")
+@app.get("/users/{user_id}", response_model=User)
 def get_user(user_id: int):
-    return {"user": [user for user in users if user.get("id") == user_id][0]} 
+    return [user for user in users if user.get("id") == user_id][0]
 
 
 trades = [
@@ -42,7 +61,7 @@ def change_user_name(user_id: int, name:str):
 class Trade(BaseModel):
     id: int
     user_id: int
-    amount: int
+    amount: int = Field(ge=0)
 
 @app.post("/trade")
 def add_trade(trade: Trade):

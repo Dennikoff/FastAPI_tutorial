ca2326fb082a4c9e5ce4bf6eacdd779166f8e872
Денис Харке <hda007@campus.mephi.ru>
2024-06-06 18:50:53 +0300

    add migrations

---
+++ models/models.py
@@ -0,0 +1,26 @@
+import datetime as dt
+from datetime import datetime 
+from sqlalchemy import JSON, Column, Integer, MetaData, String, TIMESTAMP, ForeignKey, Table
+import sqlalchemy
+
+metadata = MetaData()
+
+roles = Table(
+    "roles",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("permissions", JSON),
+)
+
+users = Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("email", String, nullable=False),
+    Column("username", String, nullable=False),
+    Column("password", String, nullable=False),
+    Column("registered_at", TIMESTAMP, default=datetime.utcnow()),
+    Column("role_id", Integer, ForeignKey("roles.id")),
+)
+
